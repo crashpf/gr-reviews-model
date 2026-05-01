27929c5512d3a3775811e3ee1fc3d2528eea15e3
crashpf <pfountas25@gmail.com>
2026-05-01 13:57:59 +0300

    changes

--- src/split.py
+++ src/split.py
@@ -1,8 +1,19 @@
-"""split.py
+"""
+split.py
 A script that splits the data (CSV) into train/test/val splits.
 
+How to use:
 python src/split.py
 """
 from pandas import pd
 from pathlib import Path
 from sklearn.model_selection import train_test_split
+
+###### Config ######
+
+INPUT_PATH = Path("data/raw/Sentiment-analysis.xlsx")
+TRAIN_PATH = Path("data/splits/train.csv")
+TEST_PATH = Path("data/splits/test.csv")
+VAL_PATH = Path("data/splits/train.csv")
+
+###### Load ######
